57103018cae04766fc5bb1a9c46f0aebfa3b9a95
Romaan Tariq <tm23834@gmail.com>
2025-09-21 17:40:11 -0400

    arrays and function to generate random number are created

--- sillyCharacter.js
+++ sillyCharacter.js
@@ -4,3 +4,18 @@ let age = 0;
 let isSuperhero = false;
 let specialPowers = [];
 let favoriteFood = "";
+
+// Arrays for random generation
+const names = ["Captain Waffles", "Mister Pickle", "Lady Pancake", "Professor Bubble"];
+const foods = ["pizza", "sushi", "ice cream", "tacos"];
+const powers = ["fly faster than light", "turn invisible", "talk to animals", "shoot spaghetti from fingers"];
+
+// Function to generate a random character description
+function generateRandomCharacter() {
+    characterName = names[Math.floor(Math.random() * names.length)];
+    age = Math.floor(Math.random() * 100) + 1; // Age between 1 and 100
+    isSuperhero = Math.random() < 0.5; // 50% chance
+    favoriteFood = foods[Math.floor(Math.random() * foods.length)];
+    specialPowers = [powers[Math.floor(Math.random() * powers.length)]];
+    updateDescription();
+}
